6eb5c8a85aa7dbbe6bcc7dcae998d94ed84aada3
Trey Evans <lewis.r.evans@gmail.com>
2022-11-27 16:14:29 -0500

    Finished data scripts.
    Ready for testing.

--- generate_broker_changes.rb
+++ generate_broker_changes.rb
@@ -11,8 +11,23 @@ XMLNS = {
 FileUtils.mkdir_p("./cv2")
 FileUtils.rm_f(Dir.glob("./cv2/*.xml"))
 
-# TODO: write reading code for enriched XML from GlueDB
-policy_crossmap = {}
+# TODO: write reading code for enriched CSV from GlueDB
+
+policy_lookups = {}
+
+CSV.foreach("gluedb_extract_results.csv", headers: true) do |row|
+  hbx_enrollment_id = row['hbx_enrollment_id']
+  if policy_lookups.has_key?(hbx_enrollment_id)
+    policy_lookups[hbx_enrollment_id][:members][row["member_id"]] = row["start_date"]
+  else
+    policy_lookups[hbx_enrollment_id] = {
+      :eg_id => row["policy_eg_id"],
+      :members => {
+        row["member_id"] => row["start_date"]
+      }
+    }
+  end
+end
 
 # Five steps here:
 # 1. Replace policy ID
@@ -43,6 +58,12 @@ Dir.glob("./enroll_cv2/*.xml").each do |f_path|
     end
     doc.xpath("//cv:affected_member[contains(cv:is_subscriber, 'false')]", XMLNS).each do |node|
       node.remove
+    end
+    doc.xpath("//cv:enrollees/cv:enrollee", XMLNS).each do |member_node|
+      member_id_node = member_node.at_xpath(".//cv:member/cv:id/cv:id", XMLNS)
+      member_id = member_id_node.content.split("#").last
+      start_date_node = member_node.at_xpath("//cv:benefit/cv:start_date", XMLNS)
+      start_date_node.content = policy_lookups[hbx_enrollment_id][:members][member_id]
     end
     File.open("./cv2/#{hbx_enrollment_id}.xml", "wb") do |f|
       f.write doc.to_xml(indent: 2)
